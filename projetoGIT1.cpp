44dd320af97a2bd7e5ae413bb8dd94817561ca79
Netinzik <167netof@gmail.com>
2019-11-22 16:20:19 -0200

    Tudo Pronto!

--- projetoGIT1.cpp
+++ projetoGIT1.cpp
@@ -11,6 +11,7 @@ int contaLetras(char letra, string texto)
 		if(letra==texto[i])
 		c++;
 	}
+	cout<<c;
 }
 
 void substituirLetra(char letra1,char letra2, string texto)
@@ -20,6 +21,7 @@ void substituirLetra(char letra1,char letra2, string texto)
 		if(texto[i]==letra1)
 			texto[i]= letra2;
 	}
+	cout<<texto;
 }
 
 /*
@@ -38,6 +40,7 @@ int contaPalavras(string palavra, string texto)
 		if(palavra==copia)
 			c++;
 	}
+	cout<<c;
 }
 
 /*
@@ -61,10 +64,49 @@ void substituirPalavras(string palavra1, string palavra2, string texto)
 				texto[d]= copia[d];
 			}
 	}
+	cout<<texto;
 }
 
 int main(){
 
 	cout << "Entre com o texto para fazer o teste:";
+	string texto;
+	getline(cin, texto);
+	cout<<"O que Você quer fazer?";
+	cout<<"1-Ver ocorrencia de caracter no texto";
+	cout<<"2-Trocar caracter no texto";
+	cout<<"3-Ver ocorrencia de palavra no texto";
+	cout<<"4-Trocar palavra no texto";
+	int i;
+	cin>>i;
+	if(i==1){
+		cout<<"Qual letra Voce Quer ver a ocorrencia no texto?";
+		char letra;
+		cin>> letra;
+		contaLetras(letra, texto);
+	}
+	if(i==2){
+		cout<<"Qual letra Voce Quer substituir no texto?";
+		char letra, letra2;
+		cin>> letra;
+		cout<<"Por qual? ";
+		cin>>letra2;
+		substituirLetras(letra, letra2, texto);
+	}
+	if(i==3){
+		cout<<"Qual palavra Voce Quer ver a ocorrencia no texto?";
+		string letra;
+		getline(cin, letra);
+		substituirLetras(letra, texto);
+	}
+	if(i==4){
+		cout<<"Qual palavra Voce Quer substituir no texto? ";
+		string letra, letra2;
+		getline(cin, letra);
+		cout<<"Por qual? ";
+		getline(cin, letra2);
+		substituirLetras(letra, letra2, texto);
+	}
+	
 	return 0;
 }
