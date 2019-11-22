9d7810b1139fc4d8dd8b65ce35c55d5256cf4e14
XxJoaoxX-16 <jv.marcolino16@gmail.com>
2019-11-22 14:59:17 -0300

    Segunda Função

--- projetoGIT1.cpp
+++ projetoGIT1.cpp
@@ -19,7 +19,11 @@ int contaLetras(char letra, string texto)
 */
 void substituirLetra(char letra1,char letra2, string texto)
 {
-	
+		for(int i=0;i<texto.length();i++)
+	{
+		if(texto[i]==letra1)
+			texto[i]= letra2;
+	}
 }
 
 /*
